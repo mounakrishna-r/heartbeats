d2e371f87a0d5d54688b0a0f155a47b3d4136059
mounakrishna <mounakrishnarouthu@gmail.con>
2025-05-07 11:16:05 +0530

    added black background

--- lib/main.dart
+++ lib/main.dart
@@ -54,65 +54,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   double _bpm = 100;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'BPM: ${_bpm.round()}',
-              style: const TextStyle(fontSize: 32, color: Colors.red),
+              _bpm.toInt().toString(),
+              style:const TextStyle(
+                color: Colors.greenAccent,
+                fontSize: 72,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            Slider(min: 40, max: 200, divisions: 160, value: _bpm, label: _bpm.round().toString(), onChanged: (v) => setState(() {
-              _bpm = v;
-              print('BPM=$v');
-            }),
+            const SizedBox(height: 40),
+            Slider(
+              value: _bpm,
+              min: 40,
+              max: 200,
+              divisions: 160,
+              label: _bpm.toInt().toString(),
+              onChanged: (double val) {
+                setState(() => _bpm = val);
+                debugPrint('Fake BPM: ${val.toInt()}');
+              },
             ),
           ],
         ),
