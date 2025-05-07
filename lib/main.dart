24343dd0e81e0cd1944ba14125bb510882e34b00
mounakrishna <mounakrishnarouthu@gmail.con>
2025-05-08 00:51:22 +0530

    live bpm updating

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:health/health.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp( MaterialApp( home: HeartRateStream()));
+
+class HeartRateStream extends StatefulWidget{
+
+  const HeartRateStream({super.key});
+
+  @override
+  State <HeartRateStream> createState() => _HeartRateStreamState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _HeartRateStreamState extends State<HeartRateStream>{
+  final HealthFactory _health = HealthFactory();
+  int? _bpm;
+  bool _authorized = false;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  Future<void> fetchBPM() async{
+    final types = [HealthDataType.HEART_RATE];
+    final now = DateTime.now();
+    final past = now.subtract(Duration(minutes: 2));
+
+    _authorized = await _health.requestAuthorization(types);
+
+    if(_authorized){
+      final data = await _health.getHealthDataFromTypes(past, now, types);
+      if(data.isNotEmpty){
+        final latest = data.last;
+        setState(() {
+          _bpm = (latest.value as NumericHealthValue).numericValue.round();});
+      }
+    } else{
+      print('X not authorized');
+    }
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  double _bpm = 100;
+  void initState(){
+    super.initState();
+    fetchBPM();
+    Future.delayed(Duration.zero, () async{
+      while(mounted) {
+        await fetchBPM();
+        await Future.delayed(Duration(seconds: 10));
+      }
+    });
+  }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              _bpm.toInt().toString(),
-              style:const TextStyle(
-                color: Colors.greenAccent,
-                fontSize: 72,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 40),
-            Slider(
-              value: _bpm,
-              min: 40,
-              max: 200,
-              divisions: 160,
-              label: _bpm.toInt().toString(),
-              onChanged: (double val) {
-                setState(() => _bpm = val);
-                debugPrint('Fake BPM: ${val.toInt()}');
-              },
-            ),
-          ],
+      appBar: AppBar(title: const Text("Live Heart Rate")),
+      body: Center(
+        child: Text(
+          _bpm !=null ? 'BPM: $_bpm' : 'waiting for data....',
+          style: TextStyle(fontSize: 32),
         ),
-      ),
+      )
     );
   }
 }
